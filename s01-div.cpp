56861624dc0e96d474095cf938abf5afd5deaa78
filip2 <s25663@pjwstk.edu.pl>
2021-10-21 13:06:38 +0200

    02-kalkulator-dzielenie

---
+++ s01-div.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <string>
+// for std :: cout , std :: cerr , and std :: cin
+auto main (int argc , char * argv []) -> int
+{
+auto const a = std::stoi ( argv [1]);
+auto const b = std::stoi ( argv [2]);
+double wyn=( (1.0*a) / (1.0*b) );
+std::cout << wyn << "\n";
+return 0;
+}
+
+//:)
