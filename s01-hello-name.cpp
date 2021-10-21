09355e4d7a6075218784b65ba59546edd3af1f48
filip2 <s25663@pjwstk.edu.pl>
2021-10-21 13:11:49 +0200

    s02-imie-po-wpisaniu

---
+++ s01-hello-name.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+auto main() -> int{
+	
+	auto name = std::string{};
+	std::getline(std::cin, name);
+	
+	std::cout << "Hello, " << name << "!\n";
+	
+	return 0;
+	
+	
+	
+	
+}
